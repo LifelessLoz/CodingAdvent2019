0d00892f1618ab91714a5b643847084a3192e0c5
LifelessLoz <38470405+LifelessLoz@users.noreply.github.com>
2019-12-02 12:14:41 +0000

    Add files via upload
    returns output to main rather than printing in class

--- Day 2/Day2.kt
+++ Day 2/Day2.kt
@@ -13,8 +13,9 @@ class Day2 (rawInput: String) {
     private val inputs: List<Int> = rawInput.split(",").map { it.toInt() }
     private var input = inputs.toMutableList()
     private val targetNumber = 19690720
+    private var nounVerbInput = 0
 
-    fun part1() {
+    fun part1(): Int {
         input[1] = 12
         input[2] = 2
         for (i in input.indices step 4) {
@@ -31,10 +32,10 @@ class Day2 (rawInput: String) {
                 throw Exception("Unexpected Value!")
             }
         }
-        print(input[0])
+        return(input[0])
     }
 
-    fun part2() {
+    fun part2(): Int {
         for(noun in 0..100) {
             for (verb in 0..100) {
                 input = inputs.toMutableList()
@@ -52,9 +53,10 @@ class Day2 (rawInput: String) {
                     }
                 }
                 if (input[0] == targetNumber)
-                    print(100 * noun + verb)
+                    nounVerbInput = 100 * noun + verb
             }
         }
+        return(nounVerbInput)
     }
 }
 
@@ -62,6 +64,8 @@ fun readFile(fileName: String)
         = File(fileName).inputStream().readBytes().toString(Charsets.UTF_8)
 
 fun main(){
-    Day2(readFile("C:\\Users\\lawre\\IdeaProjects\\Advent of code\\src\\Day2\\Input.txt")).part1()
-    Day2(readFile("C:\\Users\\lawre\\IdeaProjects\\Advent of code\\src\\Day2\\Input.txt")).part2()
+    val output = Day2(readFile("C:\\Users\\lawre\\IdeaProjects\\Advent of code\\src\\Day2\\Input.txt")).part1()
+    println(output)
+    val output2 = Day2(readFile("C:\\Users\\lawre\\IdeaProjects\\Advent of code\\src\\Day2\\Input.txt")).part2()
+    println(output2)
 }
