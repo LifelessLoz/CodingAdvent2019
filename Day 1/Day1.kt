faee438bfdd6e33efdc6fda01e9b406cf307c3e6
LifelessLoz <38470405+LifelessLoz@users.noreply.github.com>
2019-12-03 16:27:32 +0000

    Small improvements

--- Day 1/Day1.kt
+++ Day 1/Day1.kt
@@ -10,11 +10,9 @@ import java.io.File
 
 class Day1 (rawInput: List<String>){
     private val inputs: List<Int> = rawInput.map {it.toInt()}
-    private var fuelCounterUpper: Int = 0
-    private var fuelModulusCounterUpper: Int = 0
-    private var fuelInput: Int = 0
 
     fun part1(): Int {
+        var fuelCounterUpper = 0
         for (input in inputs){
             fuelCounterUpper += (input / 3) - 2
         }
@@ -22,6 +20,8 @@ class Day1 (rawInput: List<String>){
     }
 
     fun part2(): Int {
+        var fuelModulusCounterUpper = 0
+        var fuelInput = 0
         for (input in inputs){
             fuelInput = input
             while (fuelInput > 0){
@@ -38,8 +38,7 @@ fun readFile(fileName: String): List<String>
         = File(fileName).readLines()
 
 fun main(){
-    val total: Int = Day1(readFile("C:\\Users\\lawre\\IdeaProjects\\Advent of code\\src\\Day1\\Input.txt")).part1()
-    println(total)
-    val total2: Int = Day1(readFile("C:\\Users\\lawre\\IdeaProjects\\Advent of code\\src\\Day1\\Input.txt")).part2()
-    println(total2)
+    val fileName = "C:\\Users\\lawre\\IdeaProjects\\Advent of code\\src\\Day1\\Input.txt"
+    println(Day1(readFile(fileName)).part1())
+    println(Day1(readFile(fileName)).part2())
 }
