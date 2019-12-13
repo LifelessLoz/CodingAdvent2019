6a27bb1eb265d3ee0c411ff5164db304600df3ab
LifelessLoz <38470405+LifelessLoz@users.noreply.github.com>
2019-12-13 15:31:19 +0000

    Add files via upload

--- Day 7/Day7.kt
+++ Day 7/Day7.kt
@@ -1,106 +1,156 @@
-/**
- * Advent of Code 2019, Day 7 - Amplification Circuit
- *
- * Problem solved by - Lawrence Barraclough
- */
-
 package Day7
 
 import java.io.File
 import java.lang.Exception
+import kotlin.math.pow
 
-class Day7 (rawInput: String) {
-    private val inputs: List<Int> = rawInput.split(",").map { it.toInt() }
-    var input = inputs.toMutableList()
+enum class IntcodeState{
+    Running, Halted, Terminated
+}
 
-    fun pointer(numInput: Pair<Int, Int>): Int {
-        var i = 0
-        var swap = 0
-        while (true) {
-            when (input[i] % 100){
-                1 -> {
-                    input[input[i + 3]] = parameters(1, i) + parameters(2, i)
-                    i += 4
-                }
-                2 -> {
-                    input[input[i + 3]] = parameters(1, i) * parameters(2, i)
-                    i += 4
-                }
-                3 -> {
-                    if (swap == 0) {
-                        input[input[i + 1]] = numInput.first
-                        swap++
-                    }
-                    else
-                        input[input[i + 1]] = numInput.second
+class Day7 (val input: LongArray) {
+    var memTest = LongArray(10000) {0}
+    var currentState: IntcodeState = IntcodeState.Halted
+    var numInput = mutableListOf<Long>()
+
+    var i = 0
+    var b = 0
+    var output = mutableListOf<Long>()
+
+    operator fun get(address: Int) = memTest[address]
+    operator fun set(address: Int, value: Long) {
+        memTest[address] = value
+    }
+
+    init {
+        input?.copyInto(memTest)
+    }
+    var memory = memTest.toMutableList()
+
+    private fun arg(i: Int, b: Int, n: Int): Int =
+        when (val mode = memory[i].toInt() / 10.0.pow((n + 1).toDouble()).toInt() % 10) {
+            0 -> memory[i + n].toInt()
+            1 -> i + n
+            2 -> b + memory[i + n].toInt()
+            else -> error("Cannot recognise mode : $mode")
+        }
+
+    private operator fun List<Long>.get(i: Int, b: Int, n: Int): Long =
+        memory.getOrElse(arg(i, b, n)) { 0 }
+
+    private operator fun MutableList<Long>.set(i: Int, b: Int, n: Int, value: Long) {
+        val addr = arg(i, b, n)
+        if (addr < size) {
+            memory[arg(i, b, n)] = value
+        } else {
+            if (this is ArrayList) {
+                ensureCapacity(addr + 1)
+            }
+            addAll(List(addr - size) { 0L })
+            add(value)
+        }
+    }
+
+    fun pointer() {
+        when (memory[i].toInt() % 100){
+            1 -> {
+                memory[i, b, 3] = memory[i, b, 1] + memory[i, b, 2]
+                i += 4
+            }
+            2 -> {
+                memory[i, b, 3] = memory[i, b, 1] * memory[i, b, 2]
+                i += 4
+            }
+            3 -> {
+                if (numInput.isNotEmpty()) {
+                    memory[i, b, 1] = numInput[0]
+                    numInput.removeAt(0)
+                    currentState = IntcodeState.Running
                     i += 2
                 }
-                4 -> {
-                    return input[input[i + 1]]
-                }
-                5 -> {
-                    if(parameters(1, i) != 0)
-                        i = parameters(2, i)
-                    else
-                        i += 3
-                }
-                6 -> {
-                    if(parameters(1, i) == 0)
-                        i = parameters(2, i)
-                    else
-                        i += 3
-                }
-                7 -> {
-                    if(parameters(1, i) < parameters(2, i))
-                        input[input[i + 3]] = 1
-                    else
-                        input[input[i + 3]] = 0
-                    i += 4
-                }
-                8 -> {
-                    if(parameters(1, i) == parameters(2, i))
-                        input[input[i + 3]] = 1
-                    else
-                        input[input[i + 3]] = 0
-                    i += 4
-                }
-                99 -> return input[input[i + 1]]
-                else -> throw Exception("Error, Unexpected Value!")
+                else currentState = IntcodeState.Halted
             }
+            4 -> {
+                output.add(memory[i, b, 1])
+                i += 2
+            }
+            5 -> {
+                if(memory[i, b, 1] != 0L)
+                    i = memory[i, b, 2].toInt()
+                else
+                    i += 3
+            }
+            6 -> {
+                if(memory[i, b, 1] == 0L)
+                    i = memory[i, b, 2].toInt()
+                else
+                    i += 3
+            }
+            7 -> {
+                if(memory[i, b, 1] < memory[i, b, 2])
+                    memory[i, b, 3] = 1
+                else
+                    memory[i, b, 3] = 0
+                i += 4
+            }
+            8 -> {
+                if(memory[i, b, 1] == memory[i, b, 2])
+                    memory[i, b, 3] = 1
+                else
+                    memory[i, b, 3] = 0
+                i += 4
+            }
+            99 -> {
+                currentState = IntcodeState.Terminated
+                i += 1
+            }
+            else -> throw Exception("Error, Unexpected Value!")
         }
     }
 
-    private fun parameters(decider: Int, i: Int): Int{
-        var output = 0
-        var divideBy = 0
-        if (decider == 1)
-            divideBy = 100
-        if (decider == 2)
-            divideBy = 1000
-        output = if (input[i] / divideBy % 10 == 1){
-            input[i + decider]
-        } else
-            input[input[i + decider]]
-        return output
-    }
-
-    fun ampCircuitTester(input: MutableList<List<Int>>): List<Int>{
-        var output = mutableListOf<Int>()
-        for (i in input.indices)
-            output.add(ampCircuit(input[i]))
-        return output
-    }
-
-    private fun ampCircuit(phaseSetting: List<Int>): Int{
-        var output = 0
-        for (i in phaseSetting.indices){
-            output = pointer(Pair(phaseSetting[i], output) )
+        fun runIntcode(): MutableList<Long>{
+            currentState = IntcodeState.Running
+            while (currentState == IntcodeState.Running)
+                pointer()
+            return output
         }
-        return output
+}
+
+class amplifier(val rawInput: String, val input: List<Long>, repeat: Boolean){
+    private val amps: List<Day7>
+    var output = mutableListOf<Long>()
+    private val intcodeInput = rawInput.split(",").map { it.toLong() }.toLongArray()
+    var numInput = input.toMutableList()
+
+    init {
+        amps = arrayListOf (Day7(intcodeInput),
+                            Day7(intcodeInput),
+                            Day7(intcodeInput),
+                            Day7(intcodeInput),
+                            Day7(intcodeInput))
+        for (i in 0..3) {
+            amps[i + 1].numInput = amps[i].output
+            amps[i].output.add(numInput[i + 1])
+        }
+        if (repeat) {
+            amps[0].numInput = amps[4].output
+            amps[4].output.add(numInput[0])
+            amps[4].output.add(0)
+        } else {
+            amps[0].numInput.add(numInput[0])
+            amps[0].numInput.add(0)
+        }
+    }
+
+    fun runAmps() {
+        do {
+            amps.forEach { it.runIntcode() }
+        } while (!amps.all { it.currentState == IntcodeState.Terminated })
+        output = amps[4].output
     }
 }
 
-fun heapsAlgorithm(n: Int, listElements: MutableList<Int>, output: MutableList<List<Int>>){
+fun heapsAlgorithm(n: Int, listElements: MutableList<Long>, output: MutableList<List<Long>>){
     if (n == 1) {
         output.add(listElements.toList())
     }
@@ -124,9 +174,23 @@ fun readFile(fileName: String)
 
 fun main(){
     val fileName = "C:\\Users\\lawre\\IdeaProjects\\Advent of code\\src\\Day7\\Input.txt"
-    val intList = mutableListOf(0,1,2,3,4)
-    var part1 = mutableListOf<List<Int>>()
-    heapsAlgorithm(intList.size, intList, part1)
-    val results = Day7(readFile(fileName)).ampCircuitTester(part1)
-    print(results.max())
+
+    val intList1 = mutableListOf(0L,1,2,3,4)
+    val intList2 = mutableListOf(5L,6,7,8,9)
+    var part1 = mutableListOf<List<Long>>()
+    var part2 = mutableListOf<List<Long>>()
+    heapsAlgorithm(intList1.size, intList1, part1)
+    heapsAlgorithm(intList2.size, intList2, part2)
+
+    println(part1.map {
+        val thruster1 = amplifier(readFile(fileName), it, false)
+        thruster1.runAmps()
+        it to thruster1.output.joinToString("").toInt()
+    }.maxBy{it.second})
+
+    println(part2.map {
+        val thruster2 = amplifier(readFile(fileName), it, true)
+        thruster2.runAmps()
+        it to thruster2.output.joinToString("").toInt()
+    }.maxBy{it.second})
 }
